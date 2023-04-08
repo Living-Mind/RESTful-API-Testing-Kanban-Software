b969edc59562d21d1171f70bdca77509818e8655
Living-Mind <redlined_twick@aleeas.com>
2023-04-08 08:15:25 +0300

    Test: Changing card to not existing column (PATCH) + last_card function

--- test_api.py
+++ test_api.py
@@ -10,6 +10,15 @@ key = config.get("API", "key")
 url = "https://none3m.kanbanize.com/api/v2/"
 headers = {"apikey": key}
 
+def last_card():
+
+    response = requests.get(f'{url}cards', headers=headers)
+
+    last_card = response.json()["data"]["data"][0]
+
+    return last_card
+
+
 #POST METHOD (Creating new card)
 def test_new_card():
     payload = {
@@ -34,13 +43,10 @@ def test_check_card():
 
     response = requests.get(f'{url}cards', headers=headers)
 
-    #! 
-    last_card = response.json()["data"]["data"][0]
+    #print(last_card())
 
-    #print(last_card)
-
-    assert last_card["title"] == "new-card"
-    assert last_card["color"] != "34a97b"
+    assert last_card()["title"] == "new-card"
+    assert last_card()["color"] != "34a97b"
 
 
 #PATCH METHOD (Changing the column of the created card)
@@ -48,31 +54,40 @@ def test_change_column():
 
     response = requests.get(f'{url}cards', headers=headers)
 
-    #! 
-    last_card = response.json()["data"]["data"][0]
+    #print(last_card())
 
-    #print(last_card)
-
-    last_card_id = last_card["card_id"]
+    #last_card_id = last_card()["card_id"]
 
     payload = {
         "column_id": 3,
         "workflow_id": 1,
         }
 
-    response = requests.patch(f'{url}cards/{last_card_id}', headers=headers, json=payload)
+    response = requests.patch(f'{url}cards/{last_card()["card_id"]}', headers=headers, json=payload)
 
     
     response = requests.get(f'{url}cards', headers=headers)
 
-    #! 
-    last_card = response.json()["data"]["data"][0]
-    #print(last_card)
+    #print(last_card())
 
-    assert last_card["column_id"] == 3, "Card column ID should be 3"
+    assert last_card()["column_id"] == 3, "Card column ID should be 3"
 
+
+# PATCH METHOD (Changing the card to not existing column)
+def test_change_false_column():
+
+    payload = {
+        "column_id": 6
+    }
+
+    response = requests.patch(f'{url}cards/{last_card()["card_id"]}', headers=headers, json=payload)
+    
+    assert response.status_code == 200, f"Status Code: {response.status_code}"
 
 
 test_new_card()
 test_check_card()
 test_change_column()
+test_change_false_column()
+
+
